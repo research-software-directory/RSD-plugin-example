6cfdfce7eef010004a83ac5a8db07a0f5debaee8
Christian Meeßen <christian.meessen@gfz-potsdam.de>
2024-09-27 10:46:35 +0200

    feat: add dummy middleware

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # RSD-plugin-example
+
 This repo contains an example of how to create RSD plugins.
+
+## How to use
+
+1) Copy `.env.example ` to `.env`
+2) Check the network of the RSD and replace the network name in the plugin `docker-compose.yml`, if necessary.
+3) Modify nginx configuration in RSD and add a new location:
+
+   ```nginx
+   	location /plugin/dummyplugin/api {
+   		resolver 127.0.0.11 valid=30s ipv6=off;
+   		set $pluginBackend dummy-plugin-backend;
+   		proxy_pass http://$pluginBackend:8081;
+   	}
+   ```
+
+4) Modify `settings.json` in RSD-as-a-service:
+
+   ```json
+   {
+    "host": {
+        "plugins": ["dummyplugin"]
+    }
+   }
+   ```
+
+5) Rebuild nginx and frontend and start the RSD
+
+   ```bash
+   # In RSD-as-a-service repository
+   docker compose build nginx frontend
+   docker compose up
+   ```
+
+6) Build and start the plugin backend
+   
+   ```bash
+   # In the plugin repository
+   docker compose build
+   docker compose up
+   ```
---
+++ backend/main.py
@@ -0,0 +1,59 @@
+# SPDX-FileCopyrightText: 2024 Christian Meeßen (GFZ) <christian.meessen@gfz-potsdam.de>
+# SPDX-FileCopyrightText: 2024 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import uvicorn
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from os import environ
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(root_path=environ.get("API_ROOT_PATH", "/"))
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class PluginSlot(BaseModel):
+    slot: str
+    icon: str
+    href: str
+    title: str
+    subtitle: str
+
+@app.get("/config")
+async def config_endpoint(request: Request) -> list[PluginSlot]:
+    # For informative purposes
+    print(request)
+    pluginSlots = [
+        PluginSlot(
+            slot="editSoftwareNav",
+            icon='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 12.75c1.63 0 3.07.39 4.24.9 1.08.48 1.76 1.56 1.76 2.73V18H6v-1.61c0-1.18.68-2.26 1.76-2.73 1.17-.52 2.61-.91 4.24-.91M4 13c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2m1.13 1.1c-.37-.06-.74-.1-1.13-.1-.99 0-1.93.21-2.78.58C.48 14.9 0 15.62 0 16.43V18h4.5v-1.61c0-.83.23-1.61.63-2.29M20 13c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2m4 3.43c0-.81-.48-1.53-1.22-1.85-.85-.37-1.79-.58-2.78-.58-.39 0-.76.04-1.13.1.4.68.63 1.46.63 2.29V18H24zM12 6c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3"></path></svg>',
+            href="/somePath",
+            title="Software Nav Plugin Slot",
+            subtitle="",
+        ),
+        PluginSlot(
+            slot="userMenu",
+            icon='<svg viewBox="0 0 24 24"><path d="M12 12.75c1.63 0 3.07.39 4.24.9 1.08.48 1.76 1.56 1.76 2.73V18H6v-1.61c0-1.18.68-2.26 1.76-2.73 1.17-.52 2.61-.91 4.24-.91M4 13c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2m1.13 1.1c-.37-.06-.74-.1-1.13-.1-.99 0-1.93.21-2.78.58C.48 14.9 0 15.62 0 16.43V18h4.5v-1.61c0-.83.23-1.61.63-2.29M20 13c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2m4 3.43c0-.81-.48-1.53-1.22-1.85-.85-.37-1.79-.58-2.78-.58-.39 0-.76.04-1.13.1.4.68.63 1.46.63 2.29V18H24zM12 6c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3"></path></svg>',
+            href="/somePath",
+            title="User Menu Plugin Slot",
+            subtitle="",
+        ),
+    ]
+    return pluginSlots
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8080, reload=True)
